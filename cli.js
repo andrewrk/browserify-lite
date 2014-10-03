14312b84c0a8d7f44749c99b0fbe2051891fc58d
Andrew Kelley <superjoe30@gmail.com>
2014-10-03 11:50:00 -0700

    print usage when cli invoked invalidly

--- cli.js
+++ cli.js
@@ -7,16 +7,31 @@ var entrySourcePath = process.argv[2];
 var outputFileParam = process.argv[3];
 var outBundlePath = process.argv[4];
 
+if (entrySourcePath === '--help') {
+  usage();
+}
+
 if (outputFileParam !== '--outfile') {
-  console.error("Expected second param to be --outfile");
-  process.exit(1);
+  console.error("Expected second param to be --outfile\n");
+  usage();
 }
 
 if (!outBundlePath || !entrySourcePath) {
-  console.error("Expected first arg to be source path and third arg to be out bundle path.");
-  process.exit(1);
+  console.error("Expected first arg to be source path and third arg to be out bundle path.\n");
+  usage();
 }
 
 browserifyLite.createBundle(entrySourcePath, outBundlePath, function(err) {
   if (err) throw err;
 });
+
+
+function usage() {
+  console.error(
+      "Usage: browserify-lite ./entry-file.js --outfile bundle.js\n" +
+      "\n" +
+      "Standard Options:\n" +
+      "\n" +
+      "        --outfile  Write the browserify bundle to this file.");
+  process.exit(1);
+}
